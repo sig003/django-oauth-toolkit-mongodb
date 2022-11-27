97272e72c92565094d144ff8012cf6b31fb94e95
sig03 <sig03@naver.com>
2022-11-27 01:38:22 +0000

    add permission

---
+++ drf_house_test/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+class CustomReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        return obj.email == request.user.email
--- drf_house_test/views.py
+++ drf_house_test/views.py
@@ -2,24 +2,26 @@ from rest_framework import viewsets, status, generics, permissions
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
 from .models import Users, Houses
-from .serializers import UserSerializers, SelectUserSerializers, HouseSerializers
+from .serializers import UserSerializers, HouseSerializers
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from .permissions import CustomReadOnly
 
 class UsersViewSet(viewsets.ModelViewSet):
     queryset = Users.objects.all()
     serializer_class = UserSerializers
-    permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
+    #permission_classes = [IsAuthenticated, TokenHasReadWriteScope]
+    permission_classes = [CustomReadOnly]
     lookup_field = 'name'
 
     # def update(self, request, pk=None):
     #     print(pk)
     #     return Response(status=status.HTTP_204_NO_CONTENT)
 
-    # def destroy(self, request, pk=None):
-    #     item = get_object_or_404(Users, name=pk)
-    #     item.delete()
-    #     return Response(status=status.HTTP_204_NO_CONTENT)
+    def destroy(self, request, pk=None):
+        item = get_object_or_404(Users, name=pk)
+        item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 # class UserList(generics.ListAPIView):
 #     permission_classes = [AllowAny]
